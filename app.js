03933a4008942f1527cdf080b5e3efe1a96b5d4c
HerreraEG <69682690+HerreraEG@users.noreply.github.com>
2020-09-17 03:42:04 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -2,6 +2,21 @@ var fs = require('fs');
 var http = require('http');
 var dgram = require('dgram');
 var socketio = require('socket.io');
+const mysql = require('mysql');
+//Crear Conexión a la base de datos
+const database = mysql.createConnection({
+    host: 'web1.c4jg7pibawyn.us-east-1.rds.amazonaws.com',
+    user: 'admin',
+    password: 'web04diseno02',
+    database: 'web1'
+});
+//Verificacion
+database.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Connected to DB');
+});
 
 var app = http.createServer(handleRequest);
 var io = socketio.listen(app);
@@ -10,6 +25,13 @@ var socket = dgram.createSocket('udp4');
 socket.on('message', (content, rinfo) => {
     console.log(`Server got: ${content} from ${rinfo.address}:${rinfo.port}`);
     io.sockets.emit('udp message', content.toString());
+    //Enviar info a la base de datos
+    cont = content.toString().split(",")
+    cont = {latitud: cont[0], longitud: cont[1], timestamp:cont[2]}
+    let sql = 'INSERT INTO datos SET ?';
+    let query = database.query(sql, cont, (err, result) => {
+        if (err) throw err;
+    });
 });
 
 function handleRequest (req, res) {
