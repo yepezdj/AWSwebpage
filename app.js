9b9edb2ab65e9fc43e1cfe915dca51afe0195ac3
Edgar Herrera Gamarra <herreraa@uninorte.edu.co>
2020-10-01 06:22:05 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,46 +1,57 @@
 var fs = require('fs');
-var http = require('http');
 var dgram = require('dgram');
 var socketio = require('socket.io');
+var express = require('express');
 const mysql = require('mysql');
-require('dotenv').config({path: __dirname + '/.env'});
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var app = express();
+var server = require('http').Server(app);       
+var io = socketio.listen(server);
+var socket = dgram.createSocket('udp4');
 //Crear Conexión a la base de datos
 const database = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE
+    host: 'web1.c4jg7pibawyn.us-east-1.rds.amazonaws.com',
+    user: 'admin',
+    password: 'web04diseno02',
+    database: 'web1'
 });
-//Verificacion
-database.connect((err) => {
-    if (err) {
-        throw err;
-    }
-    console.log('Connected to DB');
-});
-
-var app = http.createServer(handleRequest);
-var io = socketio.listen(app);
-var socket = dgram.createSocket('udp4');
 
 socket.on('message', (content, rinfo) => {
     console.log(`Server got: ${content} from ${rinfo.address}:${rinfo.port}`);
     io.sockets.emit('udp message', content.toString());
     //Enviar info a la base de datos
     cont = content.toString().split(",")
-    cont = {latitud: cont[0], longitud: cont[1], timestamp:cont[2]}
+    cont = {lat: cont[0], lng: cont[1], timestamp:cont[2]}
     let sql = 'INSERT INTO datos SET ?';
     let query = database.query(sql, cont, (err, result) => {
         if (err) throw err;
     });
 });
 
-function handleRequest (req, res) {
+
+
+app.get('/', (request, response) => {
+    response.writeHead(200, {'content-type': 'text/html'});
     var file = fs.createReadStream('index.html');
-    file.pipe(res);
-}
+    file.pipe(response);
+});
+
+app.post('/create', urlencodedParser, function (req,res) {
+    var inicio = req.body.inicio;
+    var fin = req.body.fin;
+    inicio = inicio.toString()
+    fin = fin.toString()
+
+    let sql = `SELECT lat, lng FROM datos WHERE timestamp BETWEEN '${inicio}' and '${fin}'`;
+    let query = database.query(sql, (err, result) => {
+        if(err){ throw err;}
+        //console.log(result);
+	io.sockets.emit('historia', result);
+});
+});
 
 socket.bind(11000);
-app.listen(50000, function (){
+server.listen(50000, () => {
     console.log("Servidor en puerto 50000");
 });
