29ab51d15e1df731c5a5bcdf654bd7aa01ce3f32
HerreraEG <69682690+HerreraEG@users.noreply.github.com>
2020-10-12 00:16:57 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var app = express();
 var server = require('http').Server(app);       
 var io = socketio.listen(server);
 var socket = dgram.createSocket('udp4');
+require('dotenv').config({path: __dirname + '/.env'})
 //Crear Conexión a la base de datos
 const database = mysql.createConnection({
     host: process.env.DB_HOST,
