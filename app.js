fa665cadecb8d5480dc0d3f2de522ce5047e1c10
Edgar Herrera Gamarra <herreraa@uninorte.edu.co>
2020-10-22 03:28:25 -0500

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ socket.on('message', (content, rinfo) => {
     //Enviar info a la base de datos
     cont = content.toString().split(",")
     cont = {lat: cont[0], lng: cont[1], timestamp:cont[2]}
-    var cont3 = cont[0];
+    var cont3 = cont[3];
     if(cont3=="1"){
         let sql1 = 'INSERT INTO datos SET ?';
         let query = database.query(sql1, cont, (err, result) => {
