38ac90fbb1996043fb9254ff5f9251bab5574eba
Edgar Herrera Gamarra <herreraa@uninorte.edu.co>
2020-10-22 01:11:47 -0500

    Update app.js

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.post('/create', urlencodedParser, function (req,res) {
     fin = fin.toString()
     camion1 = camion1.toString()
     if (camion1=="on"){
-        let sql = `SELECT lat, lng FROM datos2 WHERE timestamp BETWEEN '${inicio}' and '${fin}'`;
+        let sql = `SELECT lat, lng FROM datos WHERE timestamp BETWEEN '${inicio}' and '${fin}'`;
         let query = database.query(sql, (err, result) => {
             if(err){ throw err;}
             //console.log(result);
@@ -55,7 +55,7 @@ app.post('/create', urlencodedParser, function (req,res) {
     });
     }
     else{
-        let sql = `SELECT lat, timestamp FROM datos2 WHERE timestamp BETWEEN '${inicio}' and '${fin}'`;
+        let sql = `SELECT lat, timestamp FROM datos WHERE timestamp BETWEEN '${inicio}' and '${fin}'`;
         let query = database.query(sql, (err, result) => {
             if(err){ throw err;}
             //console.log(result);
