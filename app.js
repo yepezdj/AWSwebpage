fa9ae823699e6837ee4d59fc97b05f5cb62b4035
valentinavianai <69682590+valentinavianai@users.noreply.github.com>
2020-10-22 07:02:25 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var app = express();
 var server = require('http').Server(app);       
 var io = socketio.listen(server);
 var socket = dgram.createSocket('udp4');
+app.use(express.json({ limit: '1mb' }));
 //Crear Conexión a la base de datos
 require('dotenv').config({path: __dirname + '/.env'})
 const database = mysql.createConnection({
@@ -23,14 +24,47 @@ socket.on('message', (content, rinfo) => {
     io.sockets.emit('udp message', content.toString());
     //Enviar info a la base de datos
     cont = content.toString().split(",")
+    cont3 =  cont[3];
     cont = {lat: cont[0], lng: cont[1], timestamp:cont[2]}
-    let sql = 'INSERT INTO location SET ?';
-    let query = database.query(sql, cont, (err, result) => {
-        if (err) throw err;
-    });
+    if(cont3=="1"){
+        let sql1 = 'INSERT INTO location SET ?';
+        let query = database.query(sql1, cont, (err, result) => {
+            if (err) throw err;
+        }); 
+    }
+    else{
+        let sql2 = 'INSERT INTO locations SET ?';
+        let query = database.query(sql2, cont, (err, result) => {
+            if (err) throw err;
+        }); 
+    }
 });
 
+app.post('/historical', (req, res) => {
+        var numlat = req.body.o.lat;
+        var numlng = req.body.o.lng;
 
+        numlat = parseFloat(numlat.toString());
+        numlng = parseFloat(numlng.toString());
+
+        var precision = 0.0005;
+        var legend = 4;
+        r1numlat = parseFloat(numlat) + precision;
+        r1numlat = r1numlat.toFixed(legend);
+        r2numlat = numlat - precision;
+        r2numlat = r2numlat.toFixed(legend);
+        ////////////////////////// 
+        r1numlng = parseFloat(numlng) + precision;
+        r1numlng = r1numlng.toFixed(legend);
+        r2numlng = numlng - precision;
+        r2numlng = r2numlng.toFixed(legend);
+        let sql = `SELECT * FROM location WHERE (lat BETWEEN ${r2numlat} AND ${r1numlat}) AND (lng BETWEEN ${r2numlng} AND ${r1numlng})`;
+        let query = database.query(sql, (err, result) => {
+            if (err) throw err;
+            //console.log(result);
+            res.end(JSON.stringify(result));
+        });
+});
 
 app.get('/', (request, response) => {
     response.writeHead(200, {'content-type': 'text/html'});
@@ -41,16 +75,40 @@ app.get('/', (request, response) => {
 app.post('/create', urlencodedParser, function (req,res) {
     var inicio = req.body.inicio;
     var fin = req.body.fin;
+    var camion1 = req.body.camion1;
+    var camion2 = req.body.camion2;
+    var camion3 = req.body.camion3;
     inicio = inicio.toString()
     fin = fin.toString()
+    camion1 = camion1.toString()
 
-    let sql = `SELECT lat, lng FROM location WHERE timestamp BETWEEN '${inicio}' and '${fin}'`;
-    let query = database.query(sql, (err, result) => {
-        if(err){ throw err;}
-        //console.log(result);
-    io.sockets.emit('historia', result);
-    console.log(result);
-});
+    if (camion1=="on"){
+        let sql = `SELECT lat, lng FROM location WHERE timestamp BETWEEN '${inicio}' and '${fin}'`;
+        let query = database.query(sql, (err, result) => {
+            if(err){ throw err;}
+            //console.log(result);
+        io.sockets.emit('historia', result);
+        console.log(result);
+    });
+    }
+    if (camion2=="on"){
+        let sql = `SELECT lat, lng FROM locations WHERE timestamp BETWEEN '${inicio}' and '${fin}'`;
+        let query = database.query(sql, (err, result) => {
+            if(err){ throw err;}
+            //console.log(result);
+        io.sockets.emit('historia', result);
+        console.log(result);
+    });
+    }
+    if (camion3=="on"){
+        let sql = `SELECT lat, lng FROM location WHERE timestamp BETWEEN '${inicio}' and '${fin}'`;
+        let query = database.query(sql, (err, result) => {
+            if(err){ throw err;}
+            //console.log(result);
+        io.sockets.emit('historia', result);
+        console.log(result);
+    });
+    }
 });
 
 socket.bind(11000);
