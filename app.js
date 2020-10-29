4a26cf48608cb8c1aea0482051534d4e33ef9f16
Jesus Yepez <yepezdj2014@gmail.com>
2020-10-29 01:28:27 -0500

    update

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ socket.on('message', (content, rinfo) => {
     //Enviar info a la base de datos
     cont = content.toString().split(",")
     cont3 =  cont[3];
-    cont = {lat: cont[0], lng: cont[1], timestamp:cont[2]}
+    cont = {lat: cont[0], lng: cont[1], timestamp:cont[2], axis:cont[4]}
     if(cont3=="1"){
         let sql1 = 'INSERT INTO datos SET ?';
         let query = database.query(sql1, cont, (err, result) => {
@@ -86,14 +86,14 @@ app.post('/create', (req, res) => {
     camion = camion.toString();
 
     if (camion=="Camion 1"){
-        let sql = `SELECT lat, lng, timestamp FROM datos WHERE timestamp BETWEEN '${inicio}' and '${fin}'`;
+        let sql = `SELECT lat, lng, timestamp, axis FROM datos WHERE timestamp BETWEEN '${inicio}' and '${fin}'`;
         let query = database.query(sql, (err, result) => {
             if(err){ throw err;}
         res.end(JSON.stringify(result));
         });
     }
     if (camion=="Camion 2"){
-        let sql = `SELECT lat, lng, timestamp FROM datos2 WHERE timestamp BETWEEN '${inicio}' and '${fin}'`;
+        let sql = `SELECT lat, lng, timestamp, axis FROM datos2 WHERE timestamp BETWEEN '${inicio}' and '${fin}'`;
         let query = database.query(sql, (err, result) => {
             if(err){ throw err;}
             res.end(JSON.stringify(result));
